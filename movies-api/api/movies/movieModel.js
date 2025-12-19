8ff344aaf10723ce75b5a0865facc8b195de239b
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-19 21:24:46 +0000

    Added movieModel file

---
+++ movies-api/api/movies/movieModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+  movieId: {
+    type: Number,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+
+  posterPath: {
+    type: String,
+  },
+
+  releaseDate: {
+    type: String,
+  },
+
+  voteAverage: {
+    type: Number,
+  },
+
+  overview: {
+    type: String,
+  },
+
+  
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+MovieSchema.pre("save", function (next) {
+  this.updated_at = Date.now();
+  next();
+});
+
+export default mongoose.model("Movie", MovieSchema);
