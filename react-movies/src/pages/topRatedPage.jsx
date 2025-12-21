3bd2298419120954b53b23500738ddeb77fb8929
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-21 18:31:20 +0000

    Add/remove from favorites is added to top rated movies

--- react-movies/src/pages/topRatedPage.jsx
+++ react-movies/src/pages/topRatedPage.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import MovieListPageTemplate from "../components/templateMovieListPage";
 import Spinner from "../components/spinner";
 import { getTopRatedMovies } from "../api/tmdb-api";
 import AddToPlaylistIcon from "../components/cardIcons/addToPlaylistIcon";
-
-
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import RemoveFromFavoritesIcon from "../components/cardIcons/removeFromFavorites";
+import { MoviesContext } from "../contexts/moviesContext";
 
 const TopRatedPage = () => {
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["topRated"],
     queryFn: getTopRatedMovies,
   });
+
+  const { favorites } = useContext(MoviesContext);
 
   if (isLoading) return <Spinner />;
   if (isError) {
@@ -23,7 +26,21 @@ const TopRatedPage = () => {
     <MovieListPageTemplate
       title="Top Rated Movies"
       movies={data.results}
-      action={(movie) => <AddToPlaylistIcon movie={movie} />}
+      action={(movie) => {
+        const movieId = movie.id || movie.movieId;
+        const isFavorite = favorites.some((f) => f.movieId === movieId);
+
+        return (
+          <>
+            <AddToPlaylistIcon movie={movie} />
+            {isFavorite ? (
+              <RemoveFromFavoritesIcon movie={movie} />
+            ) : (
+              <AddToFavoritesIcon movie={movie} />
+            )}
+          </>
+        );
+      }}
     />
   );
 };
