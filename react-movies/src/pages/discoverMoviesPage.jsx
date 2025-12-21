ff063da9c29eee7143b09f2d026aaccb67650a1e
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-21 18:29:07 +0000

    Add/remove from favorites is added to discover movies

--- react-movies/src/pages/discoverMoviesPage.jsx
+++ react-movies/src/pages/discoverMoviesPage.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getDiscoverMovies } from "../api/tmdb-api";
 import Spinner from "../components/spinner";
 import MovieListPageTemplate from "../components/templateMovieListPage";
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylistIcon";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import RemoveFromFavoritesIcon from "../components/cardIcons/removeFromFavorites";
+import { MoviesContext } from "../contexts/moviesContext";
 
 const DiscoverMoviesPage = () => {
   const { data, error, isPending, isError } = useQuery({
@@ -10,13 +14,10 @@ const DiscoverMoviesPage = () => {
     queryFn: getDiscoverMovies,
   });
 
-  if (isPending) {
-    return <Spinner />;
-  }
+  const { favorites } = useContext(MoviesContext);
 
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
+  if (isPending) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
 
   const movies = data.results;
 
@@ -24,7 +25,21 @@ const DiscoverMoviesPage = () => {
     <MovieListPageTemplate
       title="Discover Movies"
       movies={movies}
-      action={(movie) => null}
+      action={(movie) => {
+        const movieId = movie.id || movie.movieId;
+        const isFavorite = favorites.some((f) => f.movieId === movieId);
+
+        return (
+          <>
+            <AddToPlaylistIcon movie={movie} />
+            {isFavorite ? (
+              <RemoveFromFavoritesIcon movie={movie} />
+            ) : (
+              <AddToFavoritesIcon movie={movie} />
+            )}
+          </>
+        );
+      }}
     />
   );
 };
