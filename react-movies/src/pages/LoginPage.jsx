92a4f177818e2f6453d1fc587cff7e96e7807095
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-21 18:35:15 +0000

    layout for login page is created

--- react-movies/src/pages/LoginPage.jsx
+++ react-movies/src/pages/LoginPage.jsx
@@ -1,11 +1,15 @@
 import { useContext, useState } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 const LoginPage = () => {
   const { login, isAuthenticated } = useContext(AuthContext);
-
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
@@ -24,29 +28,50 @@ const LoginPage = () => {
   }
 
   return (
-    <>
-      <h2>Login page</h2>
-      <p>You must log in to view protected pages.</p>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "80vh",
+      }}
+    >
+      <Card sx={{ width: 400, p: 3, boxShadow: 5, borderRadius: 3 }}>
+        <CardContent>
+          <Typography variant="h5" align="center" gutterBottom>
+            Login
+          </Typography>
+          <Typography variant="body2" align="center" color="text.secondary" gutterBottom>
+            You must log in to view protected pages
+          </Typography>
 
-      <input
-        id="username"
-        placeholder="user name"
-        onChange={(e) => setUserName(e.target.value)}
-      /><br />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}>
+            <TextField
+              label="Username"
+              variant="outlined"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" color="primary" onClick={handleLogin} fullWidth>
+              Log In
+            </Button>
+          </Box>
 
-      <input
-        id="password"
-        type="password"
-        placeholder="password"
-        onChange={(e) => setPassword(e.target.value)}
-      /><br />
-
-      <button onClick={handleLogin}>Log in</button>
-
-      <p>
-        Not Registered? <Link to="/signup">Sign Up!</Link>
-      </p>
-    </>
+          <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+            Not registered? <Link to="/signup">Sign Up!</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
