d357b45d722d01bd495e53d11971fc52019636d9
Olena-Khyzhniak <20106012@mail.wit.ie>
2025-12-21 17:24:36 +0000

    Add/remove from favorites is added

--- react-movies/src/pages/favoriteMoviesPage.jsx
+++ react-movies/src/pages/favoriteMoviesPage.jsx
@@ -2,9 +2,14 @@ import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import RemoveFromFavoritesIcon from "../components/cardIcons/removeFromFavorites";
 import { MoviesContext } from "../contexts/moviesContext";
+import WriteReview from "../components/cardIcons/writeReview";
 
 const FavoriteMoviesPage = () => {
   const { favorites } = useContext(MoviesContext);
+
+   if (!favorites || favorites.length === 0) {
+    return <p style={{ textAlign: "center", marginTop: "2rem" }}>No favorite movies yet.</p>;
+  }
 
   return (
     <PageTemplate
--- react-movies/src/pages/homePage.jsx
+++ react-movies/src/pages/homePage.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import RemoveFromFavoritesIcon from "../components/cardIcons/removeFromFavorites";
 import { useQuery } from "@tanstack/react-query";
 import { getMovies } from "../api/tmdb-api";
+import { MoviesContext } from "../contexts/moviesContext";
 
 const HomePage = () => {
   const { data, error, isLoading } = useQuery({
     queryKey: ["discover"],
     queryFn: getMovies,
   });
+
+  const { favorites } = useContext(MoviesContext);
 
   if (isLoading) return <h2>Loading...</h2>;
   if (error) return <h2>Error loading movies</h2>;
@@ -17,7 +21,16 @@ const HomePage = () => {
     <PageTemplate
       title="Best Movies For You"
       movies={data.results}
-      action={(movie) => <AddToFavoritesIcon movie={movie} />}
+      action={(movie) => {
+        const movieId = movie.id || movie.movieId;
+        const isFavorite = favorites.some((f) => f.movieId === movieId);
+
+        return isFavorite ? (
+          <RemoveFromFavoritesIcon movie={movie} />
+        ) : (
+          <AddToFavoritesIcon movie={movie} />
+        );
+      }}
     />
   );
 };
